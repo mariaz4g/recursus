a83ff885b54eca50b09a8f32b1acf7f7cd1f1987
Santiago-Guiza-Zayas <132522694+Santiago-Guiza-Zayas@users.noreply.github.com>
2023-05-14 15:27:09 -0700

    yo

---
+++ index.js
@@ -0,0 +1,16 @@
+function inputCheck()
+{
+    var User = document.getElementById("1");
+    var Pass = document.getElementById("2");
+    var prompt = document.getElementById("3");
+
+    if (User.value == "209030" && Pass.value =="1234")
+    {
+        return location='home.html';
+    }
+    else
+    {
+        prompt.innerHTML = "Contraseña o usuario incorrecto";
+        return
+    }
+}
