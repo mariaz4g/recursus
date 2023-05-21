cd0263ddef5ae791fcd9c3aede351015c5af30fd
Santiago-Guiza-Zayas <132522694+Santiago-Guiza-Zayas@users.noreply.github.com>
2023-05-20 18:41:24 -0700

    Funcionalidad con cookies de sesión

--- home.js
+++ home.js
@@ -1,7 +1,50 @@
+// Set cookie
+function setCookie(name, value) {
+  document.cookie = `${name}=${encodeURIComponent(value)};path=/`;
+}
 
-    var counter = 0;
+// Get cookie
+function getCookie(name)
+{
+    const cookieString = document.cookie;
+    const cookies = cookieString.split('; ');
+
+    for (let i = 0; i < cookies.length; i++)
+    {
+        const cookie = cookies[i].split('=');
+        if (cookie[0] === name)
+        {
+        return decodeURIComponent(cookie[1]);
+        }
+    }
+  return null;
+}
+
+var counter = 0;
 
 function increaseCounter()
 {
-  counter++;
+    var quantity = document.getElementById("1").value;
+    var counterCookie = parseInt(getCookie("counterCookie"));
+    var inventario = 3;
+    if(isNaN(counterCookie))
+        counter = 0;
+    else
+        counter = counterCookie;
+
+    counter += parseInt(quantity);
+    if (counter > inventario)
+        counter = inventario;
+    else
+    if (counter < 0)
+        counter = 0;
+    console.log(counter);
+    setCookie('counterCookie', counter);
+    return location='home.html';
 }
+
+
+
+const storedCounter = getCookie('counterCookie');
+console.log(storedCounter);
+
